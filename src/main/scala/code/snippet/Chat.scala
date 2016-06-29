201fe2b6593879d29f9dead639be9a368c2397f5
Joe Barnes <barnesjd@gmail.com>
2016-06-29 08:57:51 -0500

    First slap of messages onto the DOM

--- src/main/scala/code/snippet/Chat.scala
+++ src/main/scala/code/snippet/Chat.scala
@@ -1,12 +1,18 @@
 package code.snippet
 
+import java.util.concurrent.atomic.AtomicReference
+
 import net.liftweb.http.{S, SessionVar}
 import net.liftweb.util.ClearClearable
 import net.liftweb.util.Helpers._
 
 object User extends SessionVar[Option[String]](None)
 
+case class Message(user:String, text:String)
+
 object Chat {
+  private [this] val ms = new AtomicReference(List(Message("proftom", "I'm awesome"), Message("Elsa", "Beware of the frozen heart")))
+
   private [this] def doLogin:Unit = for {
     r <- S.request if r.post_?
     name <- S.param("name")
@@ -28,7 +34,9 @@ object Chat {
     doChat
 
     (if (User.get.isDefined) ".login [class+]" #> "hide"
-    else ".chat [class+]" #> "hide") & ClearClearable
+    else ".chat [class+]" #> "hide") &
+      ClearClearable &
+      ".message *" #> ms.get().map(_.text)
   }
 
 }
