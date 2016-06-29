13ca406595adfc1ed9206112eae38417f6911db0
Joe Barnes <barnesjd@gmail.com>
2016-06-29 08:32:35 -0500

    Adding post for login

--- src/main/scala/code/snippet/Chat.scala
+++ src/main/scala/code/snippet/Chat.scala
@@ -6,7 +6,19 @@ import net.liftweb.util.Helpers._
 object User extends SessionVar[Option[String]](None)
 
 object Chat {
-  def render =
-    if(User.get.isDefined) ".login [class+]" #> "hide"
+  private [this] def doPost:Unit = for {
+    r <- S.request if r.post_?
+    name <- S.param("name")
+  } yield {
+    println(name)
+    S.redirectTo("/")
+  }
+
+  def render = {
+    doPost
+
+    if (User.get.isDefined) ".login [class+]" #> "hide"
     else ".chat [class+]" #> "hide"
+  }
+
 }
